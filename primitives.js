f85afb793bea4cfdd8e92d205afc236ae0b90f3a
Nathan Stenman <stenman@ferris.edu>
2019-04-22 12:35:07 -0400

    Update primitives.js

--- primitives.js
+++ primitives.js
@@ -12,7 +12,7 @@ AFRAME.registerPrimitive('a-river', {
     defaultComponents: {
         geometry: {primitive:'box', width:1, depth:2, height:.4},
         material: {color:'blue', opacity:.5},
-        position: {x:-2, y:-1, z:0},
+        position: {x:0, y:0, z:0}, // {x:-2, y:-1, z:0}
     },
     mappings: {
     },
@@ -22,7 +22,7 @@ AFRAME.registerPrimitive('a-food', {
     defaultComponents: {
         geometry: {primitive:'box', width:.5, depth:.5, height:.5},
         material: {color:'red', opacity:1},
-        position: {x:2, y:-1, z:0},
+        position: {x:0, y:0, z:0}, // {x:2, y:-1, z:0}
     },
     mappings: {
     },
